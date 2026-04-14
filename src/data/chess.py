da1f97d5a0eb16a2eac0855fa12b9335ec8d2c10
adrianlanchares <202107370@alu.comillas.edu>
2026-04-14 12:42:14 +0200

    todo: download chess dataset

---
+++ src/data/chess.py
@@ -0,0 +1,3 @@
+# TODO: - download ches spositions and evaluations dataset (find the right dataset (Lichess?))
+#       - preprocess the dataset to create input tensors and target values
+#       - create a PyTorch Dataset and DataLoader for the chess dataset
